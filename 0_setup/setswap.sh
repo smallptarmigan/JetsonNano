803576d3e275057591090a6669a2f9fec86ba5ba
smallptarmigan <32320750+smallptarmigan@users.noreply.github.com>
2020-03-18 17:35:04 +0900

    update

--- 0_setup/setswap.sh
+++ 0_setup/setswap.sh
@@ -5,7 +5,7 @@ echo ">> setup ip"
 sudo echo -n ">> Press ENTER to continue.(Ctrl+C to exit)"
 read flag
 
-sudo dd if=/dev/zero of =/var/swapfile bs=1024 count=$((8*1024*1024))
+sudo dd if=/dev/zero of=/var/swapfile bs=1024 count=$((4*1024*1024))
 sudo chmod 600 /var/swapfile
 sudo mkswap /var/swapfile
 sudo swapon /var/swapfile
